71c618307aaaab339180a382df6024d6486ff8fe
DayoungChoii <young12430@gmail.com>
2022-07-14 14:37:05 +0900

    [jpa example1] 프로젝트 생성

---
+++ src/test/java/jpabook/jpaexmple1/Jpaexmple1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpaexmple1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpaexmple1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
