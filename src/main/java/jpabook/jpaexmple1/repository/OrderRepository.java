9084834d7d82451116e8b7bc4c946609bcb4b4b1
DayoungChoii <young12430@gmail.com>
2022-10-19 15:25:15 +0900

    [jpa example1] 주문 서비스 개발

---
+++ src/main/java/jpabook/jpaexmple1/repository/OrderRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpaexmple1.repository;
+
+import jpabook.jpaexmple1.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    final private EntityManager em;
+
+    public void save(Order order) {
+        em.persist(em);
+    }
+
+    public Order findOne(Long id){
+        return em.find(Order.class, id);
+    }
+}
