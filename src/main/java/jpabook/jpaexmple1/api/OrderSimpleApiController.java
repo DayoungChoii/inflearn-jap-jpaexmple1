ca55da748e8632fc30b5e7677a207daa9a77ddab
DayoungChoii <young12430@gmail.com>
2022-11-07 21:35:27 +0900

    [jpa example2] 간단한 주문 조회 V2 : 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpaexmple1/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpaexmple1/api/OrderSimpleApiController.java
@@ -1,17 +1,27 @@
 package jpabook.jpaexmple1.api;
 
+import jpabook.jpaexmple1.domain.Address;
 import jpabook.jpaexmple1.domain.Order;
 import jpabook.jpaexmple1.domain.OrderSearch;
+import jpabook.jpaexmple1.domain.OrderStatus;
 import jpabook.jpaexmple1.repository.OrderRepository;
 import jpabook.jpaexmple1.service.OrderService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
+ *
  * xToOne(ManyToOne, OneToOne) 관계 최적화
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ *
  */
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +29,11 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리
+     * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -27,5 +42,34 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); //lazy 강제 초기화e
         }
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환(fetch join 사용X)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> orderV2() {
+        return orderRepository.findAllByString(new OrderSearch())
+                .stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDateTime;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order o) {
+            this.orderId = o.getId();
+            this.name = o.getMember().getName();
+            this.orderDateTime = o.getOrderDate();
+            this.orderStatus = o.getStatus();
+            this.address = o.getMember().getAddress();
+        }
     }
 }
